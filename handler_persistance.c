2d47e96344eec43810235a75b7e6631177b7ea5f
Stéphane Dai <daistephane5@gmail.com>
2024-10-08 17:20:57 +0200

    create a typedef with a ramdom info

--- handler_persistance.c
+++ handler_persistance.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct id{
+    char name[100];
+    char last_name[100];
+    int age;
+    int birthday_day;
+    int birthday_month;
+    int birthday_year;
+}Id;
 
 int main(void){
-    int a = 4;
-    int b = 7;
 
+    Id customer;
+    strcpy(customer.name, "Paul");
+    strcpy(customer.last_name, "Dupont");
+    customer.age = 40;
+    customer.birthday_day = 6;
+    customer.birthday_month = 4;
+    customer.birthday_year = 1984;
+
+    printf("the customer name %s,last_name %s, age %d, birthday %d/%d/%d, Welcomme\n ",customer.name, customer.last_name, customer.age, customer.birthday_month, customer.birthday_day, customer.birthday_year );
+    return 0;
+ 
     
 }
