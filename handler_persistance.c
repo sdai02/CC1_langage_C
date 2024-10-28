0d7faf547630aefedf0c520eda9cbc55c57356a5
Stéphane Dai <daistephane5@gmail.com>
2024-10-28 12:24:08 +0100

    creation the binary tree and add some comment

--- handler_persistance.c
+++ handler_persistance.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-//Je déclare la structure pour la creation des noeud
+//Je déclare la structure qui contient root et dans root il a comme structure left et right.
 
 typedef struct Node{
     int root;
@@ -12,24 +12,24 @@ typedef struct Node{
     struct Node* right;
 }Node;
 
-// Fonction qui crée le fichier
+// Fonction qui crée le fichier Makerfile.
 
-FILE* fptr;
+FILE* makeFile;
 void makeAFile () {
 
-    // Je crée une Makerfile avec la comme argumment write
+    // Je crée une Makerfile avec la comme argumment read.
 
-    fptr = fopen("Makefile","w");
+    makeFile = fopen("Makefile","w");
 
-    // Un condition si Makefile existe
+    // Une condition si Makefile n'existe pas.
 
-    if (fptr == NULL){
+    if (makeFile == NULL){
         printf("not existe \n");
         exit(1);
     }
 
-    // Permet de fermer le fichier et le sauvegarder
-    fclose(fptr);
+    // Permet de fermer le fichier et le sauvegarder.
+    fclose(makeFile);
  
 
 };
@@ -38,9 +38,11 @@ void makeAFile () {
 
 Node* root(int value){
 
-    printf("root = %d\n", value);
+    // Permet d'allouer de la memoire sur la table Node.
 
     Node* newValue = (Node*) malloc(sizeof(Node));
+
+    // Je deffinie les valleurs.
 
     newValue -> root = value;
 
@@ -52,60 +54,81 @@ Node* root(int value){
     
 }
 
-void shuffle (Node* table, int number) {
-    int *newAttribut = &number; 
-    root(*newAttribut);
-
-    if(number %2 == 0) {
-        printf("left = %d\n", number);
-        table -> left;
-        
-        
-    } else{
-        printf("right = %d\n", number);
-        table -> right;
-    }
-
-}   
 
 //fonction pour ajouter des noeud dans la structure 
 Node* insert(Node* table, int value) {
 
-    if (table ==  NULL && value >= 0){
+
+    // Une conditon pour que sa affiche que une seule fois root.
+
+    if (table ==  NULL){
+     
+        printf("root = %d\n", value);
         return root(value);
     }
 
+    /*Une condition que si Node* left ou bien Node* right sont vide(NULL) 
+    alors dans le root recoit en entrer left et right sinon sa fait 
+    une interation sur la fonction insert*/ 
+
     if(value %2 == 0) {
-        printf("left = %d\n", value);
-        table -> left;
-        
-        shuffle(table,value);
+        if (table -> left == NULL) {
+            printf("left = %d de root %d\n", value, table -> root);
+            table -> left = root(value);
+        } else if ( table -> right == NULL) {
+            printf("right = %d de root %d\n", value, table -> root);
+            table -> right = root(value);
+        } 
+        else {
+            insert(table -> left, value); 
+        }
         
     } else{
-        printf("right = %d\n", value);
-        table -> right;
+        if (table -> right == NULL){
+            printf("right = %d de root %d\n", value, table -> root);
+            table -> right = root(value);
+        } else if ( table -> left == NULL) {
+            printf("left = %d de root %d\n", value, table -> root);
+            table -> left = root(value);
+        } else{
+            insert(table -> right, value);
+        }
+        
 
-        shuffle(table, value);
+        
         
     }
 
     return table;
 }
 
+// Permet de vider la memoire dans les Node* left et Node* right. 
 
+void freeTable (Node* table){
+    if (table != NULL){
+    freeTable(table -> left);
+    freeTable(table -> right);
+    free(table);
+    }
+    
+}
 
+// la fonction main qui va initialiser le code
 
 int main(){
 
     makeAFile();
     Node* table = NULL;
-    table = insert(table, 2);
-    table = insert(table, 3);
-    table = insert(table, 5);
-    table = insert(table, 6);
-    table = insert(table, 7);
+    
+    // Une boucle entre 0 a 7.
 
-    free(table);
+    for (int i = 0; i <= 7; i++){
+        table = insert(table, i);
+    }
+    
+  
+
+    freeTable(table);
     return 0;
     
 };
